66fb61a296df524fbe4dd881bc07497b2c72461c
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-03-25 12:10:54 -0500

    js desing fill share

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const desingLegend = document.querySelector('.js__legend');
+const contextDesing = document.querySelector('.js__context');
+
+desingLegend.addEventListener('click', () => {
+  contextDesing.classList.toggle('collapsed');
+});
+
+
+const fillLegend = document.querySelector('.js__fill');
+const formFill = document.querySelector('.js__form');
+
+fillLegend.addEventListener('click', () => {
+  formFill.classList.toggle('collapsed');
+});
+
+
+const shareLegend = document.querySelector('.js__share');
+const contextShare = document.querySelector('.js__contextshare');
+
+shareLegend.addEventListener('click', () => {
+  contextShare.classList.toggle('collapsed');
+});
