1cc13fb7bd1c61da1e64b1c25f5baa6e8e2d62fa
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-03-25 20:48:58 -0500

    js show and hide design fill and share, mediaqueries desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,85 @@
-'use strict';
+"use strict";
 
-const desingLegend = document.querySelector('.js__legend');
-const contextDesing = document.querySelector('.js__context');
+const desingLegend = document.querySelector(".js__legend");
+const contextDesing = document.querySelector(".js__context");
 
-desingLegend.addEventListener('click', () => {
-  contextDesing.classList.toggle('collapsed');
-});
+// desingLegend.addEventLi    stener("click", (hide) => {
+//   contextDesing.classList.toggle("collapsed");
+// });
 
+function show() {
+  contextDesing.classList.remove("collapsed");
+}
+function hide() {
+  contextDesing.classList.add("collapsed");
+}
 
-const fillLegend = document.querySelector('.js__fill');
-const formFill = document.querySelector('.js__form');
+function handleClick(event) {
+  reset();
+  event.preventDefault();
+  if (contextDesing.classList.contains("collapsed")) {
+    show();
+  } else {
+    hide();
+  }
+}
 
-fillLegend.addEventListener('click', () => {
-  formFill.classList.toggle('collapsed');
-});
+desingLegend.addEventListener("click", handleClick);
 
+const fillLegend = document.querySelector(".js__fill");
+const formFill = document.querySelector(".js__form");
 
-const shareLegend = document.querySelector('.js__share');
-const contextShare = document.querySelector('.js__contextshare');
+// fillLegend.addEventListener("click", (hide) => {
+//   formFill.classList.toggle("collapsed");
+// });
 
-shareLegend.addEventListener('click', () => {
-  contextShare.classList.toggle('collapsed');
-});
+function showFill() {
+  formFill.classList.remove("collapsed");
+}
+function hideFill() {
+  formFill.classList.add("collapsed");
+}
+
+function handleClickFill(event) {
+  reset();
+  event.preventDefault();
+  if (formFill.classList.contains("collapsed")) {
+    showFill();
+  } else {
+    hideFill();
+  }
+}
+
+fillLegend.addEventListener("click", handleClickFill);
+
+const shareLegend = document.querySelector(".js__share");
+const contextShare = document.querySelector(".js__contextshare");
+
+// shareLegend.addEventListener("click", (hide) => {
+//   contextShare.classList.toggle("collapsed");
+// });
+
+function showShare() {
+  contextShare.classList.remove("collapsed");
+}
+function hideShare() {
+  contextShare.classList.add("collapsed");
+}
+
+function handleClickShare(event) {
+  reset();
+  event.preventDefault();
+  if (contextShare.classList.contains("collapsed")) {
+    showShare();
+  } else {
+    hideShare();
+  }
+}
+
+shareLegend.addEventListener("click", handleClickShare);
+
+function reset() {
+  contextDesing.classList.add("collapsed");
+  formFill.classList.add("collapsed");
+  contextShare.classList.add("collapsed");
+}
