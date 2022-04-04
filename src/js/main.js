969a88e927a4336986b800235d64b93112d3b58e
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-04-04 10:44:34 -0500

    add code js share

--- src/js/main.js
+++ src/js/main.js
@@ -144,3 +144,58 @@ function handleClick(event) {
 }
 
 btnReset.addEventListener("click", handleClick);
+
+
+/*
+input obligatorios
+
+var submitButton = document.querySelector('#submit');
+var responseURL = document.querySelector('.response');
+var form = document.querySelector('form');
+var fr = new FileReader();
+
+submitButton.addEventListener('click', loadPhoto);
+
+function sendData () {
+  var inputs = Array.from(form.elements);
+  var json = getJSONFromInputs(inputs);
+  json.photo = fr.result;
+  sendRequest(json);
+}
+
+function loadPhoto(){
+  var myFile = document.querySelector('#img-selector').files[0];
+  fr.addEventListener('load', sendData);
+  fr.readAsDataURL(myFile);
+}
+
+function getJSONFromInputs(inputs){
+  return inputs.reduce(function (acc, val) {
+    if(val.nodeName !== 'BUTTON')
+      acc[val.name] = val.value;
+    return acc;
+  }, {})
+}
+
+function sendRequest(json){
+  fetch('https://awesome-profile-cards.herokuapp.com/card/', {
+    method: 'POST',
+    body: JSON.stringify(json),
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(function(resp) { return resp.json(); })
+    .then(function(result) { showURL(result); })
+    .catch(function(error) { console.log(error); });
+}
+
+function showURL(result){
+  if(result.success){
+    responseURL.innerHTML = '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
+  }else{
+    responseURL.innerHTML = 'ERROR:' + result.error;
+  }
+}
+
+*/
